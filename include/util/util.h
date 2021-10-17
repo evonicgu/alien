fff04bda91450996b9e5304f553461e6875958d4
Evonicgu <evonicgu@gmail.com>
2021-10-18 00:20:14 +0300

    Added vecset data structure to util file

--- include/util/util.h
+++ include/util/util.h
@@ -1,6 +1,9 @@
 #ifndef ALIEN_UTIL_H
 #define ALIEN_UTIL_H
 
+#include <vector>
+#include <set>
+
 namespace alien::util {
 
     template<typename T>
@@ -21,6 +24,120 @@ namespace alien::util {
 
         bool operator()(const unsigned int lhs, unsigned int rhs) const {
             return arr[lhs] < arr[rhs];
+        }
+    };
+
+    template<typename T>
+    class vecset {
+        std::vector<T> vec;
+        std::set<unsigned int, alien::util::access_less<T>> set{{vec}};
+
+    public:
+        vecset() = default;
+
+        vecset(const std::set<T>& values) {
+            vec.reserve(values.size());
+
+            for (const T& value : values) {
+                vec.push_back(value);
+
+                set.insert(set.end(), vec.size() - 1);
+            }
+        }
+
+        vecset(std::set<T>&& values) {
+            vec.reserve(values.size());
+
+            for (const T& value : values) {
+                vec.push_back(std::move(value));
+                set.insert(set.end(), vec.size() - 1);
+            }
+        }
+
+        unsigned int push_back(const T& value) {
+            auto it = set.find(value);
+
+            if (it != set.end()) {
+                return *it;
+            }
+
+            vec.push_back(value);
+            set.insert(vec.size() - 1);
+
+            return vec.size() - 1;
+        }
+
+        unsigned int push_back(T&& value) {
+            auto it = set.find(value);
+
+            if (it != set.end()) {
+                return *it;
+            }
+
+            vec.push_back(std::move(value));
+            set.insert(vec.size() - 1);
+
+            return vec.size() - 1;
+        }
+
+        typename std::vector<T>::iterator find(const T& value) {
+            auto it = set.find(value);
+
+            if (it == set.end()) {
+                return vend();
+            }
+
+            return vbegin() + *it;
+        }
+
+        T& operator[](unsigned int index) {
+            if (index < 0 || index >= vec.size()) {
+                throw std::out_of_range("Index out of range");
+            }
+
+            return vec[index];
+        }
+
+        const T& operator[](unsigned int index) const {
+            if (index < 0 || index >= vec.size()) {
+                throw std::out_of_range("Index out of range");
+            }
+
+            return vec[index];
+        }
+
+        typename std::vector<T>::iterator vbegin() {
+            return vec.begin();
+        }
+
+        typename std::vector<T>::iterator vend() {
+            return vec.end();
+        }
+
+        typename std::vector<T>::const_iterator cvbegin() {
+            return vec.cbegin();
+        }
+
+        typename std::vector<T>::const_iterator cvend() {
+            return vec.cend();
+        }
+
+        unsigned int size() const {
+            return vec.size();
+        }
+
+        operator std::set<T>() {
+            std::set<T> out;
+
+            for (unsigned int index : set) {
+                out.insert(out.end(), std::move(vec[index]));
+            }
+
+            return out;
+        }
+
+        operator std::vector<T>() {
+            return std::move(this->vec);
         }
     };
 
