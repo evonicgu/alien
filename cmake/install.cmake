063526cda09c992efd39058c3e3f682e432947a2
Evonicgu <evonicgu@gmail.com>
2021-10-22 23:38:11 +0300

    Install utf8proc library

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -3,3 +3,9 @@ CPMAddPackage(
         GITHUB_REPOSITORY jarro2783/cxxopts
         VERSION 2.2.1
 )
+
+CPMAddPackage(
+        NAME utf8proc
+        GITHUB_REPOSITORY JuliaStrings/utf8proc
+        VERSION 2.6.1
+)
