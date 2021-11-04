5d2f6077ebe027b6f8befd082f12941d2e8e52cf
Evonicgu <evonicgu@gmail.com>
2021-11-04 17:36:19 +0300

    Implemented more util::vecset<T> methods (hinted push_back, const find method, copy & move constructos)

--- include/util/vecset.h
+++ include/util/vecset.h
@@ -9,22 +9,22 @@ namespace alien::util {
 
     template<typename T>
     struct access_less {
-        const std::vector<T>& arr;
+        const std::vector<T>* arr;
 
         using is_transparent = std::true_type;
 
-        explicit access_less(const std::vector<T>& arr) : arr(arr) {}
+        explicit access_less(const std::vector<T>* arr) : arr(arr) {}
 
         bool operator()(const T& lhs, const unsigned int rhs) const {
-            return lhs < arr[rhs];
+            return lhs < arr->at(rhs);
         }
 
         bool operator()(const unsigned int lhs, const T& rhs) const {
-            return arr[lhs] < rhs;
+            return arr->at(lhs) < rhs;
         }
 
         bool operator()(const unsigned int lhs, unsigned int rhs) const {
-            return arr[lhs] < arr[rhs];
+            return arr->at(lhs) < arr->at(rhs);
         }
     };
 
@@ -33,7 +33,7 @@ namespace alien::util {
         using set_type = std::set<unsigned int, util::access_less<T>>;
 
         std::vector<T> vec;
-        set_type set{util::access_less<T>(vec)};
+        set_type set{util::access_less<T>(&vec)};
 
     public:
         vecset() = default;
@@ -83,6 +83,32 @@ namespace alien::util {
             return vec.size() - 1;
         }
 
+        unsigned int push_back(typename set_type::iterator pos, const T& value) {
+            auto it = set.find(value);
+
+            if (it != set.end()) {
+                return *it;
+            }
+
+            vec.push_back(value);
+            set.insert(pos, vec.size() - 1);
+
+            return vec.size() - 1;
+        }
+
+        unsigned int push_back(typename set_type::iterator pos, T&& value) {
+            auto it = set.find(value);
+
+            if (it != set.end()) {
+                return *it;
+            }
+
+            vec.push_back(std::move(value));
+            set.insert(pos, vec.size() - 1);
+
+            return vec.size() - 1;
+        }
+
         typename std::vector<T>::iterator find(const T& value) {
             auto it = set.find(value);
 
@@ -91,6 +117,16 @@ namespace alien::util {
             }
 
             return vbegin() + *it;
+        }
+
+        typename std::vector<T>::const_iterator find(const T& value) const {
+            auto it = set.find(value);
+
+            if (it == set.end()) {
+                return cvend();
+            }
+
+            return cvbegin() + *it;
         }
 
         T& operator[](unsigned int index) {
@@ -117,11 +153,11 @@ namespace alien::util {
             return vec.end();
         }
 
-        typename std::vector<T>::const_iterator cvbegin() {
+        typename std::vector<T>::const_iterator cvbegin() const {
             return vec.cbegin();
         }
 
-        typename std::vector<T>::const_iterator cvend() {
+        typename std::vector<T>::const_iterator cvend() const {
             return vec.cend();
         }
 
@@ -135,6 +171,58 @@ namespace alien::util {
 
         unsigned int size() const {
             return vec.size();
+        }
+
+        vecset(const vecset<T>& other) {
+            vec = other.vec;
+
+            for (unsigned int i : other.set) {
+                set.insert(set.end(), i);
+            }
+        }
+
+        vecset(vecset<T>&& other) {
+            vec = std::move(other.vec);
+
+            for (unsigned int i : other.set) {
+                set.insert(set.end(), i);
+            }
+
+            other.set.clear();
+        }
+
+        vecset<T>& operator=(const vecset<T>& other) {
+            if (this == &other) {
+                return *this;
+            }
+
+            vec.clear();
+            set.clear();
+
+            vec = other.vec;
+            for (unsigned int i : other.set) {
+                set.insert(set.end(), i);
+            }
+
+            return *this;
+        }
+
+        vecset<T>& operator=(vecset<T>&& other) noexcept {
+            if (this == &other) {
+                return *this;
+            }
+
+            vec.clear();
+            set.clear();
+
+            vec = std::move(other.vec);
+            for (unsigned int i : other.set) {
+                set.insert(set.end(), i);
+            }
+
+            other.set.clear();
+
+            return *this;
         }
 
         explicit operator std::set<T>() {
